5ebb189621d85ffbdccea600c710511c8cf0c62b
bag1s3k <jetam.mravenec@gmail.com>
2026-03-28 19:46:01 +0100

    Add prediction of average

--- ui/generate_html.py
+++ ui/generate_html.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from datetime import datetime
 
@@ -9,7 +10,7 @@ from utils.output import style_marks
 
 logger = logging.getLogger(__name__)
 
-def create_html(context: dict["str", float]) -> None:
+def create_html(context: dict["str", tuple]) -> None:
     """Generate HTML file with the results"""
     if not context:
         return
@@ -21,10 +22,17 @@ def create_html(context: dict["str", float]) -> None:
 
     template = Template(template_html)
 
+    marks_json_data = {}
+    for subject, (average, marks) in context.items():
+        total_sum_products = sum(m[0] * m[1] for m in marks)
+        total_sum_weights = sum(m[1] for m in marks)
+        marks_json_data[subject] = (total_sum_products, total_sum_weights)
+
     with open(appconfig.path.html_output, "w", encoding="utf-8") as f:
         czech_tz = pytz.timezone("Europe/Prague")
         html_content = template.render(
             data=context,
+            marks_json=json.dumps(marks_json_data),
             title="Bakalari Averages",
             style_marks=style_marks,
             last_update=datetime.now(tz=czech_tz).strftime("%Y-%m-%d %H:%M:%S")
